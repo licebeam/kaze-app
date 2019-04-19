c1d71d56088cfdf946a5f080c6efbcb8d2a31a75
licebeam <dustinwalkerart@gmail.com>
2019-04-19 13:25:47 +0900

    adds core 100 js object

--- scraper.js
+++ scraper.js
@@ -8,10 +8,10 @@ const puppeteer = require('puppeteer');
   await page.goto('https://iknow.jp/courses/566921');
   const items = await page.$$eval('.item', cues => cues.map((item, i) => {
     const kanji = item.querySelector('.cue').innerHTML
-    const kana = item.querySelector('p[class=text] > .transliteration') ? item.querySelector('p[class=text] > .transliteration').textContent : null
+    const kana = item.querySelector('p[class=text] > .transliteration') ? item.querySelector('p[class=text] > .transliteration').textContent : 'null'
     const eng = item.querySelector('.response').textContent
-    const sen = item.querySelector('.sentence-text') ? item.querySelector('.sentence-text').textContent : null
-    return { kan: kanji, tran: kana, english: eng, sentences: sen, id: i + 1 }
+    const sen = item.querySelector('.sentence-text') ? item.querySelector('.sentence-text').textContent : 'null'
+    return { kan: kanji, tran: kana.replace(/[\])}[{(]/g, ''), english: eng, sentences: sen.replace(/[\])}[{(]/g, ''), id: i + 1 }
   }));
   console.log(items);
   await browser.close();
---
+++ src/json-data/core100.js
@@ -0,0 +1,702 @@
+[
+  {
+    kan: '行く',
+    tran: 'いく',
+    english: 'go',
+    sentences: '日曜日は図書館に行きます。にちようび は としょかん に いきます。I go to the library on Sundays.',
+    id: 1
+  },
+  {
+    kan: '見る',
+    tran: 'みる',
+    english: 'see, look at',
+    sentences: '私は絵を見るのが好きです。わたし は え を みる の が すき です。I like looking at pictures.',
+    id: 2
+  },
+  {
+    kan: '多い',
+    tran: 'おおい',
+    english: 'a lot of, many',
+    sentences: '京都にはお寺が多い。きょうと に は おてら が おおい。There are a lot of temples in Kyoto.',
+    id: 3
+  },
+  {
+    kan: '家',
+    tran: 'うち',
+    english: 'home, household',
+    sentences: '家に遊びにきてください。うち に あそび に きて ください。Please come over to my home.',
+    id: 4
+  },
+  {
+    kan: 'これ',
+    tran: 'null',
+    english: 'this, this one',
+    sentences: 'これをください。これ を ください。I\'ll have this please.',
+    id: 5
+  },
+  {
+    kan: 'それ',
+    tran: 'null',
+    english: 'that, that one',
+    sentences: 'それはとてもいい話だ。それ は とても いい はなし だ。That\'s a really nice story.',
+    id: 6
+  },
+  {
+    kan: '私',
+    tran: 'わたし',
+    english: 'I (general)',
+    sentences: '私が行きましょう。わたし が いきましょう。I\'ll go.',
+    id: 7
+  },
+  {
+    kan: '仕事',
+    tran: 'しごと',
+    english: 'work, job',
+    sentences: '3月は仕事が忙しい。3 がつ は しごと が いそがしい。I\'m busy at work in March.',
+    id: 8
+  },
+  {
+    kan: 'いつ',
+    tran: 'null',
+    english: 'when',
+    sentences: '彼女はいつ来ますか。かのじょ は いつ きます か。When is she coming?',
+    id: 9
+  },
+  {
+    kan: 'する',
+    tran: 'null',
+    english: 'do, make',
+    sentences: '友達と一緒に宿題をした。ともだち と いっしょ に しゅくだい を した。I did my homework with my friend.',
+    id: 10
+  },
+  {
+    kan: '出る',
+    tran: 'でる',
+    english: 'go out, leave',
+    sentences: '今朝は早く家を出ました。けさ は はやく いえ を でました。I left the house early this morning.',
+    id: 11
+  },
+  {
+    kan: '使う',
+    tran: 'つかう',
+    english: 'use, make use of',
+    sentences: 'このパソコンを使ってください。この パソコン を つかって ください。Please use this computer.',
+    id: 12
+  },
+  {
+    kan: '所',
+    tran: 'ところ',
+    english: 'place',
+    sentences: '私は友達の所に泊まった。わたし は ともだち の ところ に とまった。I stayed at my friend\'s place for the night.',
+    id: 13
+  },
+  {
+    kan: '作る',
+    tran: 'つくる',
+    english: 'make, create',
+    sentences: '今、朝ご飯を作っています。いま、あさ ごはん を つくって います。I\'m making breakfast now.',
+    id: 14
+  },
+  {
+    kan: '思う',
+    tran: 'おもう',
+    english: 'think',
+    sentences: '私もそう思います。わたし も そう おもいます。I think so, too. ',
+    id: 15
+  },
+  {
+    kan: '持つ',
+    tran: 'もつ',
+    english: 'have, possess',
+    sentences: '私は車を持っています。わたし は くるま を もって います。I have a car.',
+    id: 16
+  },
+  {
+    kan: '買う',
+    tran: 'かう',
+    english: 'buy',
+    sentences: '郵便局で切手を買いました。ゆうびんきょく で きって を かいました。I bought a stamp at the post office.',
+    id: 17
+  },
+  {
+    kan: '時間',
+    tran: 'じかん',
+    english: 'time, hour',
+    sentences: '今は時間がありません。いま は じかん が ありません。I don\'t have time now.',
+    id: 18
+  },
+  {
+    kan: '知る',
+    tran: 'しる',
+    english: 'know',
+    sentences: '誰もその話を知らない。だれ も その はなし を しらない。No one knows that story.',
+    id: 19
+  },
+  {
+    kan: '同じ',
+    tran: 'おなじ',
+    english: 'same, identical',
+    sentences: '彼の日本語のレベルは私と同じくらいだ。かれ の にほん ご の レベル は わたし と おなじ くらい だ。His Japanese level is about the same as mine.',
+    id: 20
+  },
+  {
+    kan: '今',
+    tran: 'いま',
+    english: 'now',
+    sentences: '彼は今、勉強しています。かれ は いま、べんきょう して います。He\'s studying now.',
+    id: 21
+  },
+  {
+    kan: '新しい',
+    tran: 'あたらしい',
+    english: 'new',
+    sentences: '彼の車は新しい。かれ の くるま は あたらしい。His car is new.',
+    id: 22
+  },
+  {
+    kan: 'なる',
+    tran: 'null',
+    english: 'become',
+    sentences: '彼は医者になりました。かれ は いしゃ に なりました。He became a doctor.',
+    id: 23
+  },
+  {
+    kan: 'まだ',
+    tran: 'null',
+    english: '(not) yet, still',
+    sentences: '宿題はまだ終わっていません。しゅくだい は まだ おわって いません。I haven\'t finished my homework yet.',
+    id: 24
+  },
+  {
+    kan: 'あと',
+    tran: 'null',
+    english: 'after',
+    sentences: '仕事のあと、映画を見た。しごと の あと、 えいが を みた。I saw a movie after work.',
+    id: 25
+  },
+  {
+    kan: '聞く',
+    tran: 'きく',
+    english: 'hear, ask, listen',
+    sentences: '彼女はラジオを聞いています。かのじょ は ラジオ を きいて います。She\'s listening to the radio.',
+    id: 26
+  },
+  {
+    kan: '言う',
+    tran: 'いう',
+    english: 'say, tell',
+    sentences: '上司が「一杯、飲もう」と言った。じょうし が 「いっぱい のもう」 と いった。My boss said: "Let\'s have a drink."',
+    id: 27
+  },
+  {
+    kan: '少ない',
+    tran: 'すくない',
+    english: 'few, little',
+    sentences: '今年は雨が少ないです。ことし は あめ が すくない です。There\'s not much rain this year.',
+    id: 28
+  },
+  {
+    kan: '高い',
+    tran: 'たかい',
+    english: 'high, tall',
+    sentences: 'これがこの町で一番高いビルです。これ が この まち で いちばん たかい ビル です。This is the tallest building in this town.',
+    id: 29
+  },
+  {
+    kan: '子供',
+    tran: 'こども',
+    english: 'child',
+    sentences: '電車で子供が騒いでいた。でんしゃ で こども が さわいで いた。Children were making a lot of noise in the train.',
+    id: 30
+  },
+  {
+    kan: 'そう',
+    tran: 'null',
+    english: 'so, that way',
+    sentences: '私もそう思います。わたし も そう おもいます。That\'s what I think too.',
+    id: 31
+  },
+  {
+    kan: 'もう',
+    tran: 'null',
+    english: 'already, yet',
+    sentences: '彼はもう帰りました。かれ は もう かえりました。He\'s already gone home.',
+    id: 32
+  },
+  {
+    kan: '学生',
+    tran: 'がくせい',
+    english: 'student',
+    sentences: '彼は真面目な学生です。かれ は まじめな がくせい です。He\'s a serious student.',
+    id: 33
+  },
+  {
+    kan: '熱い',
+    tran: 'あつい',
+    english: 'hot (to touch)',
+    sentences: 'このスープはとても熱い。この スープ は とても あつい。This soup is very hot.',
+    id: 34
+  },
+  {
+    kan: 'どうぞ',
+    tran: 'null',
+    english: 'please',
+    sentences: 'こちらへどうぞ。こちら へ どうぞ。This way please.',
+    id: 35
+  },
+  {
+    kan: '午後',
+    tran: 'ごご',
+    english: 'afternoon, p.m.',
+    sentences: '明日の午後、お客様が来る。あす の ごご おきゃくさま が くる。A guest is coming to visit tomorrow afternoon.',
+    id: 36
+  },
+  {
+    kan: '長い',
+    tran: 'ながい',
+    english: 'long',
+    sentences: '彼女の髪はとても長い。かのじょ の かみ は とても ながい。Her hair is very long.',
+    id: 37
+  },
+  {
+    kan: '本',
+    tran: 'ほん',
+    english: 'book',
+    sentences: '本を1冊買いました。ほん を 1 さつ かいました。I bought one book.',
+    id: 38
+  },
+  {
+    kan: '今年',
+    tran: 'ことし',
+    english: 'this year (colloquial)',
+    sentences: '今年はイタリアに旅行したい。ことし は イタリア に りょこう したい。I want to travel to Italy this year.',
+    id: 39
+  },
+  {
+    kan: 'よく',
+    tran: 'null',
+    english: 'often, well',
+    sentences: '彼女はよく旅行に行きます。かのじょ は よく りょこう に いきます。She often goes on holiday.',
+    id: 40
+  },
+  {
+    kan: '彼女',
+    tran: 'かのじょ',
+    english: 'she, girlfriend',
+    sentences: '彼女はOLです。かのじょ は オーエル です。She\'s an office worker.',
+    id: 41
+  },
+  {
+    kan: 'どう',
+    tran: 'null',
+    english: 'how, what',
+    sentences: 'あなたはどう思いますか。あなた は どう おもいます か。What do you think?',
+    id: 42
+  },
+  {
+    kan: '言葉',
+    tran: 'ことば',
+    english: 'word, language',
+    sentences: 'この言葉の意味が分かりません。この ことば の いみ が わかりません。I don\'t understand the meaning of this word.',
+    id: 43
+  },
+  {
+    kan: '顔',
+    tran: 'かお',
+    english: 'face',
+    sentences: '彼はタオルで顔を拭きました。かれ は タオル で かお を ふきました。He wiped his face with a towel.',
+    id: 44
+  },
+  {
+    kan: '終わる',
+    tran: 'おわる',
+    english: 'finish, end',
+    sentences: '会議は4時に終わります。かいぎ は 4 じ に おわります。The conference ends at four o\'clock.',
+    id: 45
+  },
+  {
+    kan: '一つ',
+    tran: 'ひとつ',
+    english: 'one (thing)',
+    sentences: 'それを一つください。それ を ひとつ ください。Please give me one of those.',
+    id: 46
+  },
+  {
+    kan: 'あげる',
+    tran: 'null',
+    english: 'give, offer (colloquial)',
+    sentences: 'この本、あなたにあげます。この ほん、あなた に あげます。I\'ll give you this book.',
+    id: 47
+  },
+  {
+    kan: 'こう',
+    tran: 'null',
+    english: 'like this, such',
+    sentences: 'こう小さい字は読めない。こう ちいさい じ は よめない。I can\'t read such small letters.',
+    id: 48
+  },
+  {
+    kan: '学校',
+    tran: 'がっこう',
+    english: 'school',
+    sentences: '学校は8時半に始まります。がっこう は 8 じ はん に はじまります。School starts at 8:30.',
+    id: 49
+  },
+  {
+    kan: 'くれる',
+    tran: 'null',
+    english: 'be given',
+    sentences: '友達が誕生日プレゼントをくれた。ともだち が たんじょうび プレゼント を くれた。My friend gave me a birthday present.',
+    id: 50
+  },
+  {
+    kan: '始める',
+    tran: 'はじめる',
+    english: 'start (something)',
+    sentences: 'テストを始めてください。テスト を はじめて ください。Please start the test.',
+    id: 51
+  },
+  {
+    kan: '起きる',
+    tran: 'おきる',
+    english: 'get up, get out of bed',
+    sentences: '私は毎朝6時に起きます。わたし は まいあさ 6 じ に おきます。I get up at six o\'clock every morning.',
+    id: 52
+  },
+  {
+    kan: '春',
+    tran: 'はる',
+    english: 'spring',
+    sentences: '今年の春は暖かいね。ことし の はる は あたたかい ね。It\'s warm this spring, don\'t you think?',
+    id: 53
+  },
+  {
+    kan: '午前',
+    tran: 'ごぜん',
+    english: 'morning, a.m.',
+    sentences: '午前9時のニュースです。ごぜん 9 じ の ニュース です。Now for the 9.00 a.m. news.',
+    id: 54
+  },
+  {
+    kan: '別',
+    tran: 'べつ',
+    english: 'another, different',
+    sentences: '別の本も見せてください。べつ の ほん も みせて ください。Please show me another book.',
+    id: 55
+  },
+  {
+    kan: 'どこ',
+    tran: 'null',
+    english: 'where',
+    sentences: 'あの本をどこに置きましたか。あの ほん を どこ に おきました か。Where did you put that book?',
+    id: 56
+  },
+  {
+    kan: '部屋',
+    tran: 'へや',
+    english: 'room',
+    sentences: '私の部屋は2階にあります。わたし の へや は 2 かい に あります。My room is on the second floor.',
+    id: 57
+  },
+  {
+    kan: '若い',
+    tran: 'わかい',
+    english: 'young',
+    sentences: '彼はまだ若いです。かれ は まだ わかい です。He\'s still young.',
+    id: 58
+  },
+  {
+    kan: '車',
+    tran: 'くるま',
+    english: 'car, automobile',
+    sentences: '弟が車を買った。おとうと が くるま を かった。My younger brother bought a car.',
+    id: 59
+  },
+  {
+    kan: '置く',
+    tran: 'おく',
+    english: 'put, place',
+    sentences: '彼はかばんをいすの上に置きました。かれ は かばん を いす の うえ に おきました。He put the bag on the chair.',
+    id: 60
+  },
+  {
+    kan: '住む',
+    tran: 'すむ',
+    english: 'live, reside',
+    sentences: '彼は会社の近くに住んでいる。かれ は かいしゃ の ちかく に すんで いる。He lives near his office.',
+    id: 61
+  },
+  {
+    kan: '働く',
+    tran: 'はたらく',
+    english: 'work',
+    sentences: '姉は銀行で働いています。あね は ぎんこう で はたらいて います。My big sister works at a bank.',
+    id: 62
+  },
+  {
+    kan: '難しい',
+    tran: 'むずかしい',
+    english: 'difficult',
+    sentences: 'この本は難しいですね。この ほん は むずかしい です ね。This book is difficult.',
+    id: 63
+  },
+  {
+    kan: '先生',
+    tran: 'せんせい',
+    english: 'teacher (colloquial)',
+    sentences: '私は日本語の先生になりたいです。わたし は にほん ご の せんせい に なりたい です。I want to become a Japanese teacher.',
+    id: 64
+  },
+  {
+    kan: '立つ',
+    tran: 'たつ',
+    english: 'stand, rise',
+    sentences: '彼はステージに立った。かれ は ステージ に たった。He stood on stage.',
+    id: 65
+  },
+  {
+    kan: '呼ぶ',
+    tran: 'よぶ',
+    english: 'call, name',
+    sentences: 'ウェイターを呼びましょう。ウェイター を よびましょう。Let\'s call the waiter.',
+    id: 66
+  },
+  {
+    kan: '大学',
+    tran: 'だいがく',
+    english: 'university, college',
+    sentences: '大学に行ってもっと勉強したいです。だいがく に いって もっと べんきょう したい です。I want to go to college and studymore.',
+    id: 67
+  },
+  {
+    kan: '安い',
+    tran: 'やすい',
+    english: 'cheap, inexpensive',
+    sentences: 'この服はとても安かった。この ふく は とても やすかった。These clothes were very cheap.',
+    id: 68
+  },
+  {
+    kan: 'もっと',
+    tran: 'null',
+    english: 'more',
+    sentences: 'もっと近くに来てください。もっと ちかく に きて ください。Please come closer.',
+    id: 69
+  },
+  {
+    kan: '帰る',
+    tran: 'かえる',
+    english: 'go back home',
+    sentences: '家に帰ろう。うち に かえろう。Let\'s go home.',
+    id: 70
+  },
+  {
+    kan: '分かる',
+    tran: 'わかる',
+    english: 'understand',
+    sentences: '質問の意味は分かりましたか。しつもん の いみ は わかりました か。Did you understand the meaning of the question?',
+    id: 71
+  },
+  {
+    kan: '広い',
+    tran: 'ひろい',
+    english: 'wide, big',
+    sentences: '彼の家はとても広い。かれ の いえ は とても ひろい。His house is very big.',
+    id: 72
+  },
+  {
+    kan: '数',
+    tran: 'かず',
+    english: 'number',
+    sentences: 'グラスの数が足りません。グラス の かず が たりません。We don\'t have enough number of glasses.',
+    id: 73
+  },
+  {
+    kan: '近い',
+    tran: 'ちかい',
+    english: 'near, close',
+    sentences: '駅はここから近いです。えき は ここ から ちかい です。The station is near here.',
+    id: 74
+  },
+  {
+    kan: 'そこ',
+    tran: 'null',
+    english: 'there',
+    sentences: 'そこに座ってください。そこ に すわって ください。Please sit there.',
+    id: 75
+  },
+  {
+    kan: '走る',
+    tran: 'はしる',
+    english: 'run',
+    sentences: '彼は毎晩3キロ走っています。かれ は まいばん 3 キロ はしって います。Every night he runs three kilometers.',
+    id: 76
+  },
+  {
+    kan: '入れる',
+    tran: 'いれる',
+    english: 'put in',
+    sentences: '彼はかばんに手帳を入れた。かれ は かばん に てちょう を いれた。He put the notebook in his bag.',
+    id: 77
+  },
+  {
+    kan: '教える',
+    tran: 'おしえる',
+    english: 'teach, tell',
+    sentences: '彼は数学を教えています。かれ は すうがく を おしえて います。He teaches mathematics.',
+    id: 78
+  },
+  {
+    kan: '歩く',
+    tran: 'あるく',
+    english: 'walk, go on foot',
+    sentences: '駅まで歩きましょう。えき まで あるきましょう。Let\'s walk to the station.',
+    id: 79
+  },
+  {
+    kan: '会う',
+    tran: 'あう',
+    english: 'meet',
+    sentences: 'また会いましょう。また あいましょう。Let\'s meet again.',
+    id: 80
+  },
+  {
+    kan: '書く',
+    tran: 'かく',
+    english: 'write',
+    sentences: '彼に手紙を書きました。かれ に てがみ を かきました。I wrote him a letter.',
+    id: 81
+  },
+  {
+    kan: '頭',
+    tran: 'あたま',
+    english: 'head',
+    sentences: '今朝から頭が痛い。けさ から あたま が いたい。I\'ve had a headache since this morning.',
+    id: 82
+  },
+  {
+    kan: '売る',
+    tran: 'うる',
+    english: 'sell',
+    sentences: '彼は家を売った。かれ は いえ を うった。He sold the house.',
+    id: 83
+  },
+  {
+    kan: '大好き',
+    tran: 'だいすき',
+    english: 'like (something) a lot',
+    sentences: '私は犬が大好きだ。わたし は いぬ が だいすき だ。I like dogs a lot.',
+    id: 84
+  },
+  {
+    kan: '体',
+    tran: 'からだ',
+    english: 'body, physique, physical condition',
+    sentences: '私は体が丈夫だ。わたし は からだ が じょうぶ だ。I\'m physically strong.',
+    id: 85
+  },
+  {
+    kan: '直ぐ',
+    tran: 'すぐ',
+    english: 'at once, soon',
+    sentences: '直ぐ行きます。すぐ いきます。I\'ll go right away.',
+    id: 86
+  },
+  {
+    kan: '飛ぶ',
+    tran: 'とぶ',
+    english: 'fly',
+    sentences: '鳥が飛んでいます。とり が とんで います。There\'s a bird flying.',
+    id: 87
+  },
+  {
+    kan: 'とても',
+    tran: 'null',
+    english: 'very (colloquial)',
+    sentences: 'この本はとてもおもしろい。この ほん は とても おもしろい。This book is very interesting.',
+    id: 88
+  },
+  {
+    kan: '誰',
+    tran: 'だれ',
+    english: 'who',
+    sentences: '誰と会ってみたいですか。だれ と あって みたい です か。Who would you like to meet?',
+    id: 89
+  },
+  {
+    kan: '好き',
+    tran: 'すき',
+    english: 'favorite, liked',
+    sentences: '私はワインが好きです。わたし は ワイン が すき です。I like wine.',
+    id: 90
+  },
+  {
+    kan: '読む',
+    tran: 'よむ',
+    english: 'read',
+    sentences: '彼女は雑誌を読んでいます。かのじょ は ざっし を よんで います。She\'s reading a magazine.',
+    id: 91
+  },
+  {
+    kan: '次',
+    tran: 'つぎ',
+    english: 'next',
+    sentences: '次はいつ会いましょうか。つぎ は いつ あいましょう か。When shall we meet again?',
+    id: 92
+  },
+  {
+    kan: 'あなた',
+    tran: 'null',
+    english: 'you',
+    sentences: 'これはあなたの鞄ですか。これ は あなた の かばん です か。Is this your bag?',
+    id: 93
+  },
+  {
+    kan: '飲む',
+    tran: 'のむ',
+    english: 'drink',
+    sentences: '友達とお酒を飲んでいます。ともだち と おさけ を のんで います。I\'m drinking sake with my friend.',
+    id: 94
+  },
+  {
+    kan: '古い',
+    tran: 'ふるい',
+    english: 'old',
+    sentences: '私は古い車が好きです。わたし は ふるい くるま が すき です。I like old cars.',
+    id: 95
+  },
+  {
+    kan: '質問',
+    tran: 'しつもん',
+    english: 'question',
+    sentences: '質問のある方はどうぞ。しつもん の ある かた は どうぞ。Please feel free to ask any questions.',
+    id: 96
+  },
+  {
+    kan: '今日',
+    tran: 'きょう',
+    english: 'today (colloquial)',
+    sentences: '今日は仕事がありません。きょう は しごと が ありません。I don\'t have work today.',
+    id: 97
+  },
+  {
+    kan: '友達',
+    tran: 'ともだち',
+    english: 'friend, companion (colloquial)',
+    sentences: '私には友達がたくさんいます。わたし に は ともだち が たくさん います。I have a lot of friends.',
+    id: 98
+  },
+  {
+    kan: '早い',
+    tran: 'はやい',
+    english: 'early',
+    sentences: 'まだ学校へ行くには早い時間です。まだ がっこう へ いく に は はやい じかん です。It\'s still too early to go to school.',
+    id: 99
+  },
+  {
+    kan: 'どれ',
+    tran: 'null',
+    english: 'what, which',
+    sentences: 'この中でどれが好きですか。この なか で どれ が すき です か。Which of these do you like?',
+    id: 100
+  }
+]
