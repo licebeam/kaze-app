8a3449d7afdf2baf810857917121c27fa3077a63
licebeam <dustinwalkerart@gmail.com>
2019-04-19 13:10:11 +0900

    fixes scraper to display sentences

--- scraper.js
+++ scraper.js
@@ -6,16 +6,13 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://iknow.jp/courses/566921');
-  const items = await page.$$eval('.cue-response', cues => cues.map((item, i) => {
+  const items = await page.$$eval('.item', cues => cues.map((item, i) => {
     const kanji = item.querySelector('.cue').innerHTML
     const kana = item.querySelector('p[class=text] > .transliteration') ? item.querySelector('p[class=text] > .transliteration').textContent : null
     const eng = item.querySelector('.response').textContent
-    return { kan: kanji, tran: kana, english: eng, id: i }
+    const sen = item.querySelector('.sentence-text') ? item.querySelector('.sentence-text').textContent : null
+    return { kan: kanji, tran: kana, english: eng, sentences: sen, id: i + 1 }
   }));
-
-  // const ebg = await page.$$eval('.cue', cues => cues.map((item, i) => {
-  //   return { name: item.innerHTML, id: i }
-  // }));
   console.log(items);
   await browser.close();
 })();
