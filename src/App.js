9600abec87c27a13b825a705adfa63cc46bca56e
licebeam <dustinwalkerart@gmail.com>
2019-04-22 12:58:47 +0900

    fixes global card timer

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import Card from './components/Card';
 import './App.css';
 import core100 from './json-data/core1000/core100';
+
+let timerId = null;
 class App extends Component {
-  state = { cards: core100, place: 0 }
+  state = { cards: core100, place: 0, timer: false }
 
   updatePlace = (num) => {
     this.setState({ place: this.state.place + num })
   }
 
+  startTimer = (time) => {
+    this.setState({ timer: !this.state.timer }, () => {
+      if (this.state.timer) {
+        timerId = setInterval(() => {
+          this.updatePlace(1);
+        }, time)
+      }
+      if (!this.state.timer) {
+        clearInterval(timerId)
+      }
+    })
+  }
+
   render() {
-    const { cards, place } = this.state;
+    const { cards, place, timer } = this.state;
     return (
       <div className="App">
+        <button onClick={() => this.startTimer(5000)}>{timer ? 'Stop Timer' : 'Start Timer'}</button>
         <Card cards={cards} place={place} />
         <button onClick={() => this.updatePlace(-1)}> Back </button>
         <button onClick={() => this.updatePlace(1)}>Forward</button>
