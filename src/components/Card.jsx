bfc8540e128dc0383c408a202e5aafc14bdeb323
licebeam <dustinwalkerart@gmail.com>
2019-04-22 11:58:44 +0900

    adds basic information to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
+  state = { flipped: false }
+
+  flipCard = () => {
+    this.setState({ flipped: !this.state.flipped })
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.place !== this.props.place) {
+      this.setState({ flipped: false })
+    }
+  }
   render() {
     const { cards, place } = this.props;
+    const { flipped } = this.state;
+
     return (
       <div> Check out this card
-        <div>
-          {cards[place].kan}
-        </div>
-      </div>
+        {flipped ? (
+          <div>
+            {cards[place].english}
+          </div>
+        ) : (
+            <div>
+              {cards[place].kan}
+              <br />
+              {cards[place].tran !== 'null' ? cards[place].tran : null}
+            </div>
+          )
+        }
+        <button onClick={() => this.flipCard()}>Flip</button>
+      </div >
     )
   }
 }
