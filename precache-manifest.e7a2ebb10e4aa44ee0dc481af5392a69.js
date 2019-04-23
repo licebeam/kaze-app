73a7d6160271674a6d462fa5e4a0cb776c529805
licebeam <dustinwalkerart@gmail.com>
2019-04-23 10:55:13 +0900

    Updates

---
+++ precache-manifest.e7a2ebb10e4aa44ee0dc481af5392a69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a65f154c6fd8694c720d",
+    "url": "/kaze-app/static/js/runtime~main.f0cc837f.js"
+  },
+  {
+    "revision": "2f2bca5c726434f75df6",
+    "url": "/kaze-app/static/js/main.93b66b6e.chunk.js"
+  },
+  {
+    "revision": "9e9012fa14a0fba7eba1",
+    "url": "/kaze-app/static/js/2.3701b031.chunk.js"
+  },
+  {
+    "revision": "2f2bca5c726434f75df6",
+    "url": "/kaze-app/static/css/main.93c6121f.chunk.css"
+  },
+  {
+    "revision": "03d84ca936c4d6a83c8de18c723d8f90",
+    "url": "/kaze-app/index.html"
+  }
+];
